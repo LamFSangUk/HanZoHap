fb3e836e37639b2832d33b93166edaa96f8fbfdb
parksanguk <psu9808@naver.com>
2017-01-21 17:20:51 +0900

    modify HanZo

--- HanzoHap.py
+++ HanzoHap.py
@@ -3,11 +3,11 @@
 
 from operator import itemgetter
 
-def win_late(name):
+
+def win_rate(name):
     champ_ID = {}
     champ_comb = {}
-    
-    with open("HanzoHap.bin", "rb") as Hap:
+    with open("test.bin", "rb") as Hap:
         bytebuffer = bytearray(Hap.read()).decode()
         byte_list = bytebuffer.split("\n")
         for line in byte_list:
@@ -15,31 +15,28 @@ def win_late(name):
                 champ_line = line.split("|")
             except:
                 pass
-            champ_word = ""
-    
-            for sword in champ_line[0:5]:
-                champ_word += sword + " "
-    
             for champ in champ_line[0:5]:
-                word = champ_word.replace(champ, "")
-                word = word.replace("  ", " ")
-                word = word.strip()
-                    
-                Id = int(champ)
+                champion = champ_line[0:5]
+                champion.remove(champ)
+                champ_tmp = champion
+                champ_str = '|'.join(champion)
+                try:
+                    Id = int(champ)
+                    rate = float(champ_line[-1])
+                except:
+                    pass
                 if Id not in champ_ID:
                     champ_ID[Id] = list()
-                
-                if word not in champ_comb:
-                    champ_comb[word] = list()
-                
-                tp = (word, champ_line[-1])
+
+                if champ_str not in champ_comb:
+                    champ_comb[champ_str] = list()
+                tp = (champ_tmp, rate)
                 champ_ID[Id].append(tp)
-                tp = (champ, champ_line[-1])
-                champ_comb[word].append(tp)
-   
-    
+                tp = (champ, rate)
+                champ_comb[champ_str].append(tp)
     result = sorted(champ_ID[name], key=itemgetter(1))
-    
+    print(result)
+
     try:
         print(result[0:5])
     except:
