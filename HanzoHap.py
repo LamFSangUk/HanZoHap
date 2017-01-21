4e4bd6667ad36cd3177252f8ec605d532d60ca65
20151571 <dbdydgur2244@gmail.com>
2017-01-21 10:17:45 +0900

    delete json and add main file

--- HanzoHap.py
+++ HanzoHap.py
@@ -1,47 +1,46 @@
 #!/usr/bin/python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from operator import itemgetter
-champ_ID = {}
-champ_comb = {}
 
-with open("test.bin", "rb") as Hap:
-    byteBuffer = bytearray(Hap.read()).decode()
-    byte_list = byteBuffer.split("\n")
-    for line in byte_list:
-        try:
-            champ_line = line.split("|")
-            
+def win_late(name):
+    champ_ID = {}
+    champ_comb = {}
+    
+    with open("HanzoHap.bin", "rb") as Hap:
+        bytebuffer = bytearray(Hap.read()).decode()
+        byte_list = bytebuffer.split("\n")
+        for line in byte_list:
+            try:
+                champ_line = line.split("|")
+            except:
+                pass
             champ_word = ""
-
+    
             for sword in champ_line[0:5]:
                 champ_word += sword + " "
-
+    
             for champ in champ_line[0:5]:
-                
-                word = champ_word.replace(champ,"")
+                word = champ_word.replace(champ, "")
                 word = word.replace("  ", " ")
                 word = word.strip()
-
-                ID = int(champ)
-                tp = (word, champ_line[-1])
-                if ID not in champ_ID:
-                    champ_ID[ID] = []
-                champ_ID[ID].append(tp)  
+                    
+                Id = int(champ)
+                if Id not in champ_ID:
+                    champ_ID[Id] = list()
                 
-                if word not in  champ_comb:
-                    champ_comb[word] = []
-    
-                tp =  (champ, champ_line[-1])
+                if word not in champ_comb:
+                    champ_comb[word] = list()
+                
+                tp = (word, champ_line[-1])
+                champ_ID[Id].append(tp)
+                tp = (champ, champ_line[-1])
                 champ_comb[word].append(tp)
-        except: 
-            pass
-
-print(champ_comb)
-
-name = int(input())
-result = sorted(champ_ID[name], key = itemgetter(1))
-try:
-    print(result[0:5])
-except:
-    print(result)
+   
+    
+    result = sorted(champ_ID[name], key=itemgetter(1))
+    
+    try:
+        print(result[0:5])
+    except:
+        print(result)
---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.5
+#-*- coding: utf-8 -*-
+'''
+This is main file for sogang hackerton HanZoHap team
+'''
+
+import crawler
+
+
