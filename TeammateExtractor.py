cde5a4952b04750ce5791e22ea93cae23ef1c6c6
Ray Kim <msca8h@naver.com>
2017-01-21 14:19:06 +0900

    updated function call interface

--- TeammateExtractor.py
+++ TeammateExtractor.py
@@ -3,39 +3,39 @@ import requests
 
 getJSON = lambda response : response.json()
 
-SummonerIdURL = lambda region, name, key : "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + name + "?api_key=" + key
+SummonerIdURL = lambda requestData : "https://" + requestData[0] + ".api.pvp.net/api/lol/" + requestData[0] + "/v1.4/summoner/by-name/" + requestData[1] + "?api_key=" + requestData[2]
 
 CurrentGameURL = lambda region, SumID, key : "https://" + region + ".api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/"+ region + "/" + SumID + "?api_key=" + key
 
-
-
-getJSONData = lambda URL  : getJSON(requests.get(URL))
+requestJSON = lambda URL  : getJSON(requests.get(URL))
 
 refineName = lambda name : (name.lower()).replace(" ", "")
 
-getSummonerID = lambda region, name, key : getJSONData(SummonerIdURL(region, name, key))[refineName]["id"]
+getSummonerID = lambda requestData : requestJSON(SummonerIdURL(requestData))[refineName(requestData[1])]["id"]
 
-getCurrentGame = lambda region, name, key : getJSONData(CurrentGameURL(region, str(getSummonerID(region, name, key)), key))
+getCurrentGame = lambda requestData : requestJSON(CurrentGameURL(requestData[0], str(getSummonerID(requestData)), requestData[2]))
 
-getParticipants = lambda region, name, key : getCurrentGame(region, name, key)["participants"]
+getParticipants = lambda requestData : getCurrentGame(requestData)["participants"]
 
 getTeamId = lambda name, participants :  participants[0]["teamId"] if  participants[0]["summonerName"] == name else getTeamId(name, participants[1:])
 
 
-def getSameTeam(region, name, key) :
+def getSameTeam(requestData) :
    try:
-      participants = getParticipants(region, name, key)
-      teamId = getTeamId(name, participants)
-      return list(map(lambda element : (element["summonerName"], element["championId"]), list(filter(lambda element : element["teamId"] == teamId and element["summonerName"] != name , participants))))
+      participants = getParticipants(requestData)
+      teamId = getTeamId(requestData[1], participants)
+      return list(map(lambda element : (element["summonerName"], element["championId"]), list(filter(lambda element : element["teamId"] == teamId and element["summonerName"] != requestData[1] , participants))))
    except:
       return []
-	  
+
+
+   ### requestData is a tuple of (region, SummonerName, API Key)!! All strings!!
    # getSameTeam("kr", "Nagne123", "RGAPI-9d1b6e21-7e34-4c01-9b20-a26041518479") yields,
    # ['Nagne123', 'Kf beyo', '내가강만식', '코잇컴짱좋네', '셔적적셔']
    
    # exceptions, errors result in empty list
       
-#--------------------------------------------------------------------------------------------------	
+   #--------------------------------------------------------------------------------------------------	
 	  
 summonerStatsURL = lambda region, SumID, season, key : "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.3/stats/by-summoner/" + SumID + "/ranked?season=SEASON" + season + "&api_key=" + key
 	  
