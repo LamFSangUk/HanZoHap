28f3c65e107320211dcc4b2afbafe1c81da90dac
20151571 <dbdydgur2244@gmail.com>
2017-01-21 23:29:45 +0900

    m

--- testcase_gen.py
+++ testcase_gen.py
@@ -3,12 +3,12 @@ import random
 num_len = int(input('range : '))
 file_name = 'sample.bin'
 
-with open(file_name, 'wb') as fw:
+with open(file_name, 'w') as fw:
     for a in range(1, num_len+1):
         for b in range(a+1, num_len+1):
             for c in range(b+1, num_len+1):
                 for d in range(c+1, num_len+1):
                     for e in range(d+1, num_len+1):
                         line = str(a)+ '|' + str(b) + '|'+ str(c) + '|' + str(d) + '|' + str(e) + '|' + str(random.random()*100) + '\n'
-                        print(line, fw)
-                        print(line)
+                        fw.write(line)
+                        
