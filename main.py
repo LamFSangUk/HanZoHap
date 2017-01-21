7d7be0c56dc034466a0cb370ab322a9a66dfba9b
20151571 <dbdydgur2244@gmail.com>
2017-01-21 10:48:55 +0900

    m

--- main.py
+++ main.py
@@ -5,5 +5,17 @@ This is main file for sogang hackerton HanZoHap team
 '''
 
 import crawler
+from HanZoHap import win_late
 
+crawler.SummonerNameCrawler()
+with open("names.txt", "r", encoding='utf-8') as F:
+    f_lines = F.readlines()
+    print(lines)
+    for line in f_lines:
+        print(line)
+        UserId = crawler.SummonerIdCrawler(line)
+        Match_List = set()
+        Match_List.union(crawler.MatchListCrawler(UserId))
+        crawler.MatchInfoCrawler(Match_List)
+        
 
