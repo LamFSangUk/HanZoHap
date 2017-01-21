16a85f2edd3fdd4805f4e1c1769ab540456579c8
Ray Kim <msca8h@naver.com>
2017-01-21 17:34:19 +0900

    Finished

--- TeammateExtractor.py
+++ TeammateExtractor.py
@@ -1,5 +1,5 @@
-import json
 import requests
+from functools import partial
 
 getJSON = lambda response : response.json()
 
@@ -20,33 +20,57 @@ getParticipants = lambda requestData : getCurrentGame(requestData)["participants
 getTeamId = lambda name, participants :  participants[0]["teamId"] if  participants[0]["summonerName"] == name else getTeamId(name, participants[1:])
 
 
-def getSameTeam(requestData) :
+def getSameTeam(requestData):
    try:
       participants = getParticipants(requestData)
       teamId = getTeamId(requestData[1], participants)
       return list(map(lambda element : (element["summonerName"], element["championId"]), list(filter(lambda element : element["teamId"] == teamId and element["summonerName"] != requestData[1] , participants))))
    except:
       return []
-
-
-   ### requestData is a tuple of (region, SummonerName, API Key)!! All strings!!
-   # getSameTeam("kr", "Nagne123", "RGAPI-9d1b6e21-7e34-4c01-9b20-a26041518479") yields,
-   # ['Nagne123', 'Kf beyo', '내가강만식', '코잇컴짱좋네', '셔적적셔']
-   
-   # exceptions, errors result in empty list
-      
-   #--------------------------------------------------------------------------------------------------	
+	  
+	  
 	  
 summonerStatsURL = lambda region, SumID, season, key : "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.3/stats/by-summoner/" + SumID + "/ranked?season=SEASON" + season + "&api_key=" + key
 	  
 getSummonerStats = lambda requestData, season :   requestJSON(summonerStatsURL(requestData[0], str(getSummonerID(requestData)), season, requestData[2]))
 
-computeStatsFromJSON = lambda 
+getChampions = lambda data : data["champions"]
 
-getChampData = lambda 
+findChampion = lambda id, champs : champs[0] if champs[0]["id"] == id else findChampion(id, champs[1:])
 
-getTeammateStats = lambda region, name, season, key : 
+getStatsByChamp = lambda id, data : findChampion(id, getChampions(data))["stats"]
+
+getSummonerChampStats = lambda requestData, season, id  : getStatsByChamp(id, getSummonerStats(requestData, season))
+
+def computeWinStats(stats):
+   lost = stats["totalSessionsLost"]
+   won = stats["totalSessionsWon"]
+   total = lost + won
+   return ((won / lost), total)
    
    
+def makeTuple(requestData, season, elem):
+   try:
+      result = computeWinStats(getSummonerChampStats((requestData[0], elem[0], requestData[2]), season, elem[1]))
+   except:
+      result = (0,0)
+   return (elem[0], result)
+
    
+getTeammateWinStats = lambda requestData, season : list(map(partial(makeTuple, requestData, season,), getSameTeam(requestData)))
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------
+####               SYNTAX: getTeammateWinStats((region, UserName, API-Key), Season)                         ####   
+
+
+
+def main():
+   requestData = ("kr", "KDM Edge", "RGAPI-9d1b6e21-7e34-4c01-9b20-a26041518479")
+   print(getTeammateWinStats(requestData, "2016"))
+
+if __name__ == '__main__':
+    main()
 
