058ee7f3086486c77c21c13c82e281f3890f4a96
Joey <rlaalsdn0506@gmail.com>
2017-01-21 21:47:32 +0900

    Update testcase_gen.py

--- testcase_gen.py
+++ testcase_gen.py
@@ -9,4 +9,6 @@ with open(file_name, 'wb') as fw:
             for c in range(b+1, num_len+1):
                 for d in range(c+1, num_len+1):
                     for e in range(d+1, num_len+1):
-                        print(str(a) + '|' + str(b) + '|' + str(c) + '|' + str(d) + '|' + str(e) + '|' + str(random.random()*100.), file=fw)
+                        line = str(a)+ '|' + str(b) + '|'+ str(c) + '|' + str(d) + '|' + str(e) + '|' + str(random.random()*100) + '\n'
+                        print(line, fw)
+                        print(line)
